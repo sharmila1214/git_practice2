1fb9881634248eacacd993e5692ce2ce1102c558
sharmila1214 <sweety.1428@gmail.com>
2020-04-14 13:49:49 -0700

    first commit

---
+++ src/git_practice/Git_practice_demo.java
@@ -0,0 +1,17 @@
+package git_practice;
+
+public class Git_practice_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("my name is sharmila");
+System.out.println("i have two kids ");
+System.out.println("we r living in sandiego ");
+System.out.println("my first kid nam is sreetan ");
+System.out.println("my second kid nam is sreehita ");
+System.out.println("i am house owner ");
+System.out.println("my hometoen is andhra in india");
+System.out.println("my hubby working in qualcom ");
+	}
+
+}
